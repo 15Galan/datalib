d86439e6949f94541997e2e770cb3d173e50db9a
Galán <antoniogalanG15@gmail.com>
2023-01-02 17:47:06 +0100

    Funciones de impresión implementadas

---
+++ datalib/dll_print.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dll.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
+/*   Updated: 2023/01/02 12:53:50 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+void	dll_print(t_dlist *list)
+{
+	t_dlist	*aux;
+
+	aux = dll_first(list);
+	ft_putstr_fd("[ ", 1);
+	while (aux)
+	{
+		ft_putstr_fd((char *) aux->content, 1);
+		if (aux->next)
+			ft_putstr_fd(" <--> ", 1);
+		aux = aux->next;
+	}
+	ft_putstr_fd(" ]", 1);
+}
