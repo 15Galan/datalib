2fd08777e798f0f580203ababd3d2a0ab3b56e6c
Sr. Galán <antoniogalanG15@gmail.com>
2023-10-30 18:21:20 +0100

    Nueva estructura de los conjuntos
    Se modularizó el archivo de cabeceras.
    Se ordenaron las funciones en nuevos ficheros.

---
+++ inc/set.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 18:14:41 by antgalan          #+#    #+#             */
+/*   Updated: 2023/10/30 18:19:54 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SET_H
+# define SET_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "nodes.h"
+
+/** SET **/
+
+// Creates set items
+
+t_n1	*set_new(void *data);
+void	set_add(t_n1 **set, void *data);
+
+// Modifies set items (math operations)
+
+t_n1	*set_union(t_n1 *set_a, t_n1 *set_b);
+t_n1	*set_inter(t_n1 *set_a, t_n1 *set_b);
+t_n1	*set_diff(t_n1 *set_a, t_n1 *set_b);
+
+// Deletes set items
+
+void	set_remove(t_n1 **set, void *data);
+void	set_clear(t_n1 **set);
+
+// Checks set information
+
+int		set_empty(t_n1 *set);
+int		set_contains(t_n1 *set, void *data);
+int		set_subset(t_n1 *set_a, t_n1 *set_b);
+int		set_equal(t_n1 *set_a, t_n1 *set_b);
+int		set_size(t_n1 *set);
+
+// Prints the set
+
+void	set_print(t_n1 *set);
