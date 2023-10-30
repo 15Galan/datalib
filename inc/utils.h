197701b066189bb2c8b339ddd5245529f67798e6
Sr. Galán <antoniogalanG15@gmail.com>
2023-10-30 19:14:09 +0100

    Cabeceras `datalib.h` sustituidas por unas más genéricas
    El propósito de estas cabeceras es que aporten funcionalidades generales
    que se usan en las implementaciones de las estructuras de datos.

--- inc/utils.h
+++ inc/utils.h
@@ -1,27 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   datalib.h                                          :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/30 18:52:33 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/30 19:11:02 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DATALIB_H
-# define DATALIB_H
+#ifndef UTILS_H
+# define UTILS_H
 
 # include <stdlib.h>
 # include <unistd.h>
-# include "dll.h"
-# include "cdll.h"
-# include "queue.h"
-# include "set.h"
-# include "stack.h"
 
-/** GENERAL PURPOSE	**/
+/** UTILS & GENERAL PURPOSE	**/
 
 int		len(void *data);
 
