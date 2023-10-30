254d095e5b31cfd0ff879cb6a5ef17064173338b
Sr. Galán <antoniogalanG15@gmail.com>
2023-10-30 18:12:57 +0100

    Colas re-implementadas
    Nueva estructura de las colas.
    Funciones adaptadas a los nodos genéricos.

--- src/queue/print.c
+++ src/queue/print.c
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   que_printers.c                                     :+:      :+:    :+:   */
+/*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/28 23:01:25 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/30 18:00:35 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
 
+/**
+ * @brief	Prints the queue.
+ * 
+ * @param queue	The first element of the queue.
+ */
 void	que_print(t_queue *queue)
 {
 	t_queue	*aux;
