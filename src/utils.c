cdf3813688782e3d4946f3167c24c4884bbe0e3d
Sr. Galán <antoniogalanG15@gmail.com>
2023-10-30 20:38:51 +0100

    Comentarios de documentación añadidos en `len()`

--- src/utils.c
+++ src/utils.c
@@ -1,17 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   len.c                                              :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:00:35 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/28 23:00:53 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:29:45 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
 
+/**
+ * @brief	Counts the number of characters occupied by the string
+ *			representation of a data value. 
+ * 
+ * @param data	The data to represent as a string.
+ *
+ * @return	The length of the data representation as a string.
+ */
 int	len(void *data)
 {
 	int	i;
