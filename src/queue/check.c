cd21d114e16dce0649a9d273200c6e31d4e0e47a
Sr. Galán <antoniogalanG15@gmail.com>
2023-10-31 18:40:54 +0100

    Documentación de las colas movida (#2)

--- src/queue/check.c
+++ src/queue/check.c
@@ -6,32 +6,17 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/31 00:57:42 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:40:08 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/data_structures/queue.h"
 
-/**
- * @brief	Checks if the queue is empty.
- *
- * @param queue	The first item of the queue.
- *
- * @return	1 if the queue is empty;
- * 			0 otherwise.
- */
 int	que_is_empty(t_n1 *queue)
 {
 	return (queue == NULL);
 }
 
-/**
- * @brief   Counts the number of items in the queue.
- *
- * @param queue   The first item of the queue.
- *
- * @return  Number of items in the queue.
- */
 int	que_size(t_n1 *queue)
 {
 	int	i;
@@ -45,25 +30,11 @@ int	que_size(t_n1 *queue)
 	return (i);
 }
 
-/**
- * @brief   Returns the first item of the queue.
- *
- * @param queue	The first item of the queue.
- *
- * @return	The first item of the queue.
- */
 t_n1	*que_first(t_n1 *queue)
 {
 	return (queue);
 }
 
-/**
- * @brief   Returns the last item of the queue.
- *
- * @param queue	The first item of the queue.
- *
- * @return	The last item of the queue.
- */
 t_n1	*que_last(t_n1 *queue)
 {
 	while (queue && queue->next)
--- src/queue/create.c
+++ src/queue/create.c
@@ -6,19 +6,12 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/30 20:40:24 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:39:19 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/data_structures/queue.h"
 
-/**
- * @brief   Creates a new item with the given data.
- *
- * @param data  The data to be stored in the item.
- *
- * @return  The new item.
- */
 t_n1	*que_new(void *data)
 {
 	t_n1	*new;
@@ -31,12 +24,6 @@ t_n1	*que_new(void *data)
 	return (new);
 }
 
-/**
- * @brief   Adds a new item at the end of the queue.
- *
- * @param queue	The first item of the queue.
- * @param data	The data to be stored in the item.
- */
 void	que_enqueue(t_n1 **queue, void *data)
 {
 	t_n1	*new;
