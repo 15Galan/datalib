47c30cee20e7cba57c6c91e2ff81958f3178f6ee
Sr. Galán <antoniogalanG15@gmail.com>
2023-10-31 18:46:31 +0100

    Documentación de las funciones genéricas movida (#2)

--- src/utils.c
+++ src/utils.c
@@ -6,20 +6,12 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:00:35 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/30 20:50:41 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:43:50 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/utils.h"
 
-/**
- * @brief	Counts the number of characters occupied by the string
- *			representation of a data value. 
- * 
- * @param data	The data to represent as a string.
- *
- * @return	The length of the data representation as a string.
- */
 int	len(void *data)
 {
 	int	i;
